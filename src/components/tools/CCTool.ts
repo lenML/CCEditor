47a108702ceb3075bdc45395ee0f6bfe3cec9580
zhzluke96 <zhzluke96@outlook.com>
2026-04-14 03:30:54 +0800

    fix: erasableSyntaxOnly

--- src/components/tools/CCTool.ts
+++ src/components/tools/CCTool.ts
@@ -6,6 +6,11 @@
  * 3. 应该是无副作用的不会修改 card，但是可以输出新 card
  */
 export abstract class CCTool {
-  constructor(public name: string, public desc: string) {}
+  public name: string;
+  public desc: string;
+  constructor(name: string, desc: string) {
+    this.name = name;
+    this.desc = desc;
+  }
   abstract component: React.FC<any>;
 }
